d6fe9099b04890d78acaafc97111acf037cc370a
Bruno de Souza da Silva <bruno7317@gmail.com>
2025-03-31 19:32:32 -0600

    Removes static S3 hosting

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,43 +17,6 @@ resource "aws_s3_bucket" "cloudweave_s3_bucket" {
   force_destroy = true
 }
 
-resource "aws_s3_bucket_public_access_block" "cloudweave_bucket_public_access" {
-  bucket = aws_s3_bucket.cloudweave_s3_bucket.bucket
-
-  block_public_acls = false
-  block_public_policy = false
-  ignore_public_acls = false
-  restrict_public_buckets = false
-}
-
-resource "aws_s3_bucket_policy" "allow_all_access" {
-  bucket = aws_s3_bucket.cloudweave_s3_bucket.bucket
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = "*"
-      Action = [
-        "s3:GetObject"
-      ]
-      Resource = "${aws_s3_bucket.cloudweave_s3_bucket.arn}/*"
-    }]
-  })
-  
-}
-
-resource "aws_s3_bucket_website_configuration" "cloudweave_s3_website_configuration" {
-  bucket = aws_s3_bucket.cloudweave_s3_bucket.bucket
-
-  index_document {
-    suffix = "index.html"
-  }
-
-  error_document {
-    key = "error.html"
-  }
-}
-
 resource "aws_iam_role" "codepipeline_role" {
   name_prefix = "cloudweave-codepipeline"
   assume_role_policy = jsonencode({
