0ad9922dc0bff607001409b2f45a67ea1be3ffef
Bruno de Souza da Silva <bruno7317@gmail.com>
2025-04-21 18:35:29 -0600

    ArgoCD Image updater

--- scripts/bootstrap.ps1
+++ scripts/bootstrap.ps1
@@ -3,5 +3,10 @@ aws eks update-kubeconfig --name cloudweave-eks-cluster --region ca-central-1
 $argoPath = Join-Path $PSScriptRoot "..\k8s\argo"
 
 kubectl apply -f "$argoPath\namespace.yaml"
+kubectl apply -f "$argoPath\git-credentials.yaml"
+kubectl apply -f "$argoPath\image-updater-config.yaml"
+kubectl apply -f "$argoPath\repo-public.yaml"
+
 kubectl apply -n argocd -f "$argoPath\install.yaml"
+kubectl apply -n argocd -f "$argoPath\image-updater.yaml"
 kubectl apply -f "$argoPath\app-cloudweave.yaml"
